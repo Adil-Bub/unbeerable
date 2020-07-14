fa1c7644d2ad770dc4690339a99d0ed1ce7b402b
Adil-Bub <adilahmed1090@gmail.com>
2020-07-15 00:18:21 +0530

    Updates

---
+++ precache-manifest.f1328e8d400ae2426fd8f14d5e365380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506b2a41d1d92dd88d569fa318f89be7",
+    "url": "/unbeerable/index.html"
+  },
+  {
+    "revision": "6da4dd62adc1f35dc7d1",
+    "url": "/unbeerable/static/css/main.8e551e76.chunk.css"
+  },
+  {
+    "revision": "15d38f53bb0ee1fedbe3",
+    "url": "/unbeerable/static/js/2.deba2794.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unbeerable/static/js/2.deba2794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da4dd62adc1f35dc7d1",
+    "url": "/unbeerable/static/js/main.690d1901.chunk.js"
+  },
+  {
+    "revision": "78a2d3ee5dfe837b31ac",
+    "url": "/unbeerable/static/js/runtime-main.81c44593.js"
+  }
+]);
